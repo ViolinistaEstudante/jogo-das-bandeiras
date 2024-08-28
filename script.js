157b6262bb00b0e4a55a8c005167ae3cc78f4db7
ViolinistaEstudante <132099734+ViolinistaEstudante@users.noreply.github.com>
2024-08-28 18:13:12 -0300

    Jogo das bandeiras finalizado!!

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const flags = [
+const easyFlags = [
+    { src: '--', name: 'example' },
     { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Flag_of_Latvia.svg/255px-Flag_of_Latvia.svg.png', name: 'Letônia' },
     { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Flag_of_Kazakhstan.svg/800px-Flag_of_Kazakhstan.svg.png', name: 'Cazaquistão' },
     { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Bulgaria.svg/255px-Flag_of_Bulgaria.svg.png', name: 'Bulgária' },
@@ -9,9 +10,26 @@ const flags = [
     { src: 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Flag_of_Cyprus.svg', name: 'Chipre' },
     { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flag_of_Morocco.svg/640px-Flag_of_Morocco.svg.png', name: 'Marrocos' },
     { src: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Flag_of_Madagascar.svg', name: 'Madagascar' },
-];
+];  
+
+const hardFlags = [
+    { src: '--', name: 'example'},
+    { src: 'https://recreio.com.br/media/uploads/legacy/2020/01/16/bandeiras-1210923.png', name: 'Ilha de Man'},
+    { src: 'https://recreio.uol.com.br/images/large/2020/01/16/bandeiras-1210926.jpg', name: 'Frísia'},
+    { src: 'https://recreio.uol.com.br/images/large/2020/01/16/bandeiras-1210922.png', name: 'Quirguistão'},
+    { src: 'https://recreio.uol.com.br/images/large/2020/01/16/bandeiras-1210921.png', name: 'Ilha de Guan'},
+    { src: 'https://recreio.uol.com.br/images/large/2020/01/16/bandeiras-1210927.png', name: 'Veneto'},
+    { src: 'https://recreio.uol.com.br/images/large/2020/01/16/bandeiras-1210920.png', name: 'Martinica'},
+    { src: 'https://recreio.uol.com.br/images/large/2020/01/16/bandeiras-1210924.png', name: 'Ulster'},
+    { src: 'https://hypescience.com/wp-content/uploads/2009/10/bandeira-3.jpg', name: 'Império de Benin'},
+    { src: 'https://hypescience.com/wp-content/uploads/2009/10/bandeira-6.gif', name: 'Antuérpia' },
+    { src: 'https://hypescience.com/wp-content/uploads/2009/10/bandeira-13.gif', name: 'Lombardia' },
+
+]
 
 let currentFlagIndex = 0;
+let flags = easyFlags; // Default to easy level
+
 
 function loadNextFlag() {
     if (currentFlagIndex < flags.length - 1) {
@@ -40,3 +58,14 @@ function checkAnswer() {
 }
 
 window.onload = loadNextFlag;
+
+function changeLevel() {
+    const level = document.getElementById('level').value;
+    if (level === 'easy') {
+        flags = easyFlags;
+    } else {
+        flags = hardFlags;
+    }
+    currentFlagIndex = 0;
+    loadNextFlag();
+}
